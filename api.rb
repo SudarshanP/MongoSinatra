cbe692444b2070502ebce552498d5aeaf4d38518
Sudarshan Palliyil <ibm.sudarshan@gmail.com>
2011-12-02 19:37:21 +0530

    forgot to add

---
+++ api.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require 'uri'
+require 'mongo'
+
+class MyAPI < Sinatra::Base
+	get '/' do
+	   uri = URI.parse(ENV['MONGOHQ_URL'])
+	   conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+	   db = conn.db(uri.path.gsub(/^\//, ''))
+	   coll = db['bookmarks']
+	   
+	   ret = "Failed"
+	   ret = "Hello World from Sinatra & mongodb:" if db
+	   coll.find().each { |row| ret += row.inspect } if coll
+	   ret += "No bookmarks" unless coll
+	   ret
+	end
+
+	get '/env' do
+	   ENV.inspect
+	end
+end
