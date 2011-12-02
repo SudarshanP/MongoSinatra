26e2c2bd9dfc3262192aba144664af440480a2ab
Sudarshan Palliyil <ibm.sudarshan@gmail.com>
2011-12-02 17:16:03 +0530

    sinatra mongo5

--- hello.rb
+++ hello.rb
@@ -6,8 +6,10 @@ get '/' do
    uri = URI.parse(ENV['MONGOHQ_URL'])
    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
    db = conn.db(uri.path.gsub(/^\//, ''))
+   coll = db['bookmarks']
    
    ret = "Failed"
    ret = "Hello World from Sinatra & mongodb:" if db
+   ret = ret + coll.count
    ret
 end
