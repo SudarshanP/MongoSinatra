2a34bbef984088e2393bd29fe74b57329c161987
Sudarshan Palliyil <ibm.sudarshan@gmail.com>
2011-12-02 16:53:25 +0530

    sinatra mongo2

--- hello.rb
+++ hello.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World from Sinatra!"
+   db = Connection.new(ENV['DATABASE_URL']).db(db_name)
+   if ENV['DATABASE_USER'] && ENV['DATABASE_PASSWORD']
+      auth = db.authenticate(ENV['DATABASE_USER'], ENV['DATABASE_PASSWORD'])
+   end
+  "Hello World from Sinatra & mongodb:" + auth
 end
